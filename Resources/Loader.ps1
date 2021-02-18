98769c1d414e07617cce0386366e9ac8a1f87750
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-02-18 10:55:53 +0800

    Convert from SF7N to ABP loader

--- Resources/Loader.ps1
+++ Resources/Loader.ps1
@@ -21,18 +21,13 @@ $xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]").Name.
     ForEach({$wpf.Add($_, $tempform.FindName($_))})
 
 # Import GUI Control functions
-# Write-Log 'INF' 'Import GUI Control Modules'
-# Import-Module "$baseLocation\Functions\SF7N-UX.ps1",
+Import-Module "$baseLocation\ABP-Import.ps1"
 #     "$baseLocation\Functions\SF7N-Edit.ps1",
 #     "$baseLocation\Functions\SF7N-Search.ps1",
 #     "$baseLocation\SF7N-GUI.ps1"
 
 # Cleanup on close
-# $wpf.SF7N.Add_Closing({
-#     Write-Log 'DBG'
-#     Write-Log 'INF' 'Remove Modules'
-#     # Remove-Module 'SF7N-*'
-# })
+$wpf.ABP.Add_Closing({Remove-Module 'ABP-*'})
 
 # Load WPF >> Using method from https://gist.github.com/altrive/6227237
 $wpf.ABP.Dispatcher.InvokeAsync({$wpf.ABP.ShowDialog()}).Wait() | Out-Null
