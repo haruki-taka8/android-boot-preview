14122699d7bd7d11e65ac09119bab991c8ceb379
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-02-19 21:46:10 +0800

    Initial commit, Season 2

---
+++ Loader.ps1
@@ -0,0 +1,32 @@
+<#
+    Cloned from SammyKrosoft/Powershell/How-To-Load-WPF-Form-XAML.ps1
+    Modified & used under the MIT License (https://github.com/SammyKrosoft/PowerShell/blob/master/LICENSE.MD)
+#>
+#—————————————————————————————————————————————————————————————————————————————+—————————————————————
+# VARIABLES
+$ffmpegLocation = 'ffmpeg.exe'
+$TempLocation = "$PSScriptRoot\Temp\"
+
+# Basic settings
+Set-Location $PSScriptRoot
+Add-Type -AssemblyName PresentationFramework, PresentationCore, System.Windows.Forms
+
+# Load a WPF GUI from a XAML file
+[Xml] $xaml = Get-Content GUI.xaml
+$tempform = [Windows.Markup.XamlReader]::Load([System.Xml.XmlNodeReader]::New($xaml))
+$wpf = [Hashtable]::Synchronized(@{})
+$xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]").Name.
+    ForEach({$wpf.Add($_, $tempform.FindName($_))})
+
+# Import GUI Control functions
+Import-Module "$PSScriptRoot\ABP-Import.ps1",
+              "$PSScriptRoot\ABP-Generate.ps1"
+
+# Cleanup on close
+$wpf.ABP.Add_Closing({
+    $wpf.Image_Preview.Source = $null
+    Remove-Module 'ABP-*'
+})
+
+# Load WPF >> Using method from https://gist.github.com/altrive/6227237
+$wpf.ABP.Dispatcher.InvokeAsync({$wpf.ABP.ShowDialog()}).Wait() | Out-Null
