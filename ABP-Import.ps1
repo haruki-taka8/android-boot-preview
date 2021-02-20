976d2864644e36c234f3d4d0556a5042e52c25ff
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-02-20 11:08:11 +0800

    Add calls to Set-Progress; Remove unused code

--- ABP-Import.ps1
+++ ABP-Import.ps1
@@ -23,6 +23,7 @@ $wpf.Button_Foldername.Add_Click({
 # Import file
 $wpf.Button_Import.Add_Click({
     $Extracted    = $false
+    Set-Progress 0 'Extracting boot animation'
     if (($wpf.TextBox_Filename.Text -ne '') -and (Test-Path $wpf.TextBox_Filename.Text)) {
         # Extract .zip
         $Extracted = $true
@@ -37,6 +38,7 @@ $wpf.Button_Import.Add_Click({
     }
 
     if ($Extracted) {
+        Set-Progress 50 'Comprehending desc.txt'
         $wpf.TextBox_Desc.Text = Get-Content $tempLocation\desc.txt -Raw
         # Prepare $desc variable from desc.txt
         $script:desc = [PSCustomObject] @{}
@@ -79,61 +81,6 @@ $wpf.Button_Import.Add_Click({
             $i++
         })
     }
+
+    Set-Progress 100 'Import done'
 })
-
-
-
-# Import file
-<#
-$wpf.Button_Import.Add_Click({
-    if (Test-Path $TextBox_Filename.Text) {
-        # Unzip
-        $script:TempZip = "$PSSCriptRoot\tempZip"
-        If (Test-Path $TempZip) {Remove-Item "$TempZip\*" -Recurse}
-        Expand-Archive $TextBox_Filename.Text $TempZip -Force
-
-        # Show desc.txt content
-        $script:desc = [PSCustomObject] @{}
-        $desc | Add-Member NoteProperty Text (Get-Content $TempZip\desc.txt -Raw)
-        $TextBox_desc.Text = $desc.Text
-
-        # Fill $desc with data from desc.txt
-        #    .Text = Raw content
-        #    .Width
-        #    .Height
-        #    .FPS
-        #    .<Ratio (W/H)>
-        #    .<Image prefix>
-        #    .<FrameInterval>
-        #    .Animation
-        #        [0]
-        #        .Type
-        #        .Count
-        #        .Pause
-        #        .Path
-        $FirstLine = (Get-Content $TempZip\desc.txt -First 1) -Split ' '
-        $desc | Add-Member NoteProperty Width  $FirstLine[0]
-        $desc | Add-Member NoteProperty Height $FirstLine[1]
-        $desc | Add-Member NoteProperty FPS    $FirstLine[2]
-        $desc | Add-Member NoteProperty Animation ([System.Collections.ArrayList] @())
-        
-        $desc | Add-Member NoteProperty Interval (1000/$desc.FPS)
-        $desc | Add-Member NoteProperty Ratio ($desc.Width/$desc.Height)
-        $TempPrefix = (Get-ChildItem $TempZip\part0).name | Select-Object -First 1
-        $desc | Add-Member NoteProperty Prefix $TempPrefix.Substring(0, $TempPrefix.Length-5)
-
-
-        (Get-Content $TempZip\desc.txt | Select-Object -Skip 1).ForEach({
-            $ThisLine = $_ -Split ' '
-            $desc.Animation.Add(
-                [PSCustomObject] @{
-                    Type   = $ThisLine[0]
-                    Repeat = $ThisLine[1]
-                    Pause  = $ThisLine[2]
-                    Path   = $ThisLine[3]
-                }
-            )
-        })
-    }
-})
-#>
