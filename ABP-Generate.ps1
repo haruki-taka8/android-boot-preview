d98fd39ee6242e5b58700d49ff331c3cd945cf2e
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-02-20 22:55:23 +0800

    Rewrite; Add support for P Type

--- ABP-Generate.ps1
+++ ABP-Generate.ps1
@@ -8,68 +8,125 @@ $wpf.Button_Generate.Add_Click({
     $OverlayX = ($ScreenW - $desc.Width) / 2
     $OverlayY = ($ScreenH - $desc.Height) / 2
 
-    # Foreach part
-    Set-Progress 33 'Generating animation per part'
-    for ($i = 0; $i -lt $desc.Animation.Count; $i++) {
-        # Generate .avi for part
-        # Generate Animation
+    # Generate animation per line
+    $script:i = 0
+    $desc.Animation.ForEach({
+        Set-Progress (100*($i+1)/($desc.Animation.Count+2)) 'Generating animation per part'
+
+        # Generate overlay
         '#' | Out-File $tempLocation\partList.txt -Encoding ASCII
-        (Get-ChildItem "$tempLocation\part$i\*.png").FullName.ForEach({
+        (Get-ChildItem "$tempLocation\$($_.Path)\*.png").FullName.ForEach({
             "file `'$_`'" | Out-File $tempLocation\partList.txt -Append -Encoding ASCII
         })
 
         & $ffmpegLocation `
-            -r 1 `
+            -f concat `
+            -safe 0 `
+            -r $desc.FPS `
+            -i "$tempLocation\partList.txt" `
+            -r $desc.FPS `
+            "$tempLocation\$($i)_front.avi"
+
+        # Process REPEAT
+        $RequiredRepeat = $desc.Animation[$i].Repeat
+        if ($RequiredRepeat -eq 0) {
+            $RequiredRepeat = $wpf.TextBox_Repeat.Text
+        }
+
+        Clear-Content $tempLocation\partList.txt
+        for ($j = 0; $j -lt $RequiredRepeat; $j++) {
+            "file `'$tempLocation\$($i)_front.avi`'" | Out-File $tempLocation\partList.txt -Append -Encoding ASCII
+        }
+
+        & $ffmpegLocation `
             -f concat `
             -safe 0 `
             -i "$tempLocation\partList.txt" `
-            -r $desc.FPS `
-            "$tempLocation\part$($i)_front.avi"
+            "$tempLocation\$($i)_front_2.avi"
 
-        # Add blank background
-        # Get background color
+        # Append overlay to background
         if ($null -eq $desc.Animation[$i].RGBHex[0]) {
             $script:RequiredColor = '#000000'
         } else {
             $script:RequiredColor = $desc.Animation[$i].RGBHex[0]
         }
 
-        $RequiredPartTime = 2 * $desc.Animation[$i].PartTime
-        if ($i -eq $desc.Animation.Count-1) {
-            $RequiredPartTime += $desc.Animation[$i].Pause / $desc.FPS
-        }
+        # Set Animation.FullTime (include PAUSE & REPEAT)
+        $desc.Animation[$i].FullTime = $desc.Animation[$i].PartTime*$RequiredRepeat +$desc.Animation[$i].Pause/$desc.FPS 
 
         & $ffmpegLocation `
             -f lavfi `
             -i color=$($RequiredColor):s=$($ScreenW)x$($ScreenH) `
-            -i "$tempLocation\part$($i)_front.avi" `
+            -i "$tempLocation\$($i)_front_2.avi" `
             -filter_complex "[0:v][1:v] overlay=$($OverlayX):$($OverlayY)" `
-            -t $RequiredPartTime `
-            -r $desc.FPS `
-            "$tempLocation\part$($i).avi"
-    }
+            -t $desc.Animation[$i].FullTime `
+            "$tempLocation\$($i).avi"
 
-    # Merge all
-    # Generate list of files to merge
-    Set-Progress 66 'Merging animations'
-    [System.Collections.ArrayList] $Part = @()
+        # Remove unused $i_front_*.avi
+        Remove-Item "$TempLocation\$($i)_front*.avi" -Force
+        $i++
+    })
+
+    # Process BOOTTIME
+    $script:CurrentPosition = 0 # ms
+    $script:PreviousPosition = 0 # ms
+    $script:Cutoff = 0
+    $script:i = 0
+
     $desc.Animation.ForEach({
-        if ($_.Repeat -eq 0) {
-            $RequiredRepeat = $wpf.TextBox_Repeat.Text
-        } else {
-            $RequiredRepeat = $_.Repeat
+        $CurrentPosition += $desc.Animation[$i].FullTime*1000 # ms
+
+        if ($CurrentPosition -gt $wpf.TextBox_Boot.Text) {
+
+            if (($desc.Animation[$i].Type -ieq 'c') -and ($desc.Animation[$i].Repeat -eq 0)) {
+                # Process Infinite C loop
+                $script:Cutoff = [Math]::Round(($wpf.TextBox_Boot.Text - $PreviousPosition) / ($desc.Animation[$i].PartTime*1000)) * $desc.Animation[$i].PartTime*1000
+                $script:Cutoff = [Math]::Max(0, $Cutoff)
+                $script:Cutoff = [Timespan]::FromMilliseconds($CutOff).ToString('hh\:mm\:ss\.ff')
+
+                & $ffmpegLocation `
+                    -i "$tempLocation\$($i).avi" `
+                    -ss '00:00:00' `
+                    -to $CutOff `
+                    -c copy `
+                    "$tempLocation\$($i)_2.avi"
+
+                Copy-Item "$tempLocation\$($i)_2.avi" "$tempLocation\$($i).avi" -Force
+                Remove-Item "$tempLocation\$($i)_2.avi" -Force
+
+            } elseif ($desc.Animation[$i].Type -ieq 'p') {
+                # Process P loop
+                $script:Cutoff = [Math]::Max(0, $wpf.TextBox_Boot.Text - $PreviousPosition)
+                $script:Cutoff = [Timespan]::FromMilliseconds($CutOff).ToString('hh\:mm\:ss\:ff')
+
+                & $ffmpegLocation `
+                    -i "$tempLocation\$($i).avi" `
+                    -ss '00:00:00' `
+                    -to $CutOff `
+                    -c copy `
+                    "$tempLocation\$($i)_2.avi"
+
+                Copy-Item "$tempLocation\$($i)_2.avi" "$tempLocation\$($i).avi" -Force
+                Remove-Item "$tempLocation\$($i)_2.avi" -Force
+            }
         }
-        for ($i = 0; $i -lt $RequiredRepeat; $i++) {
-            $Part.Add($_)
-        }
+
+        $PreviousPosition += $desc.Animation[$i].FullTime*1000 # ms
+        $i++
     })
 
-    '#' | Out-File $tempLocation\partList.txt -Encoding ASCII
-    $Part.Path.ForEach({
-        "file `'$tempLocation\$_.avi`'" | Out-File $tempLocation\partList.txt -Append -Encoding ASCII
+    # Slow down
+    # Look at the time up on the wall
+    # The clock is tellin' it all wrong
+    # You got to slow down
+    Start-Sleep 2
+
+    # Combine each part
+    Clear-Content $tempLocation\partList.txt
+    (Get-ChildItem "$tempLocation\*.avi").FullName.ForEach({
+        "file `'$_`'" | Out-File $tempLocation\partList.txt -Append -Encoding ASCII
     })
 
-    # The moment of truth: combine everything
     & $ffmpegLocation `
             -f concat `
             -safe 0 `
