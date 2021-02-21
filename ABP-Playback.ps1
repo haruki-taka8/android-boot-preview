f652db6e8bca30b72d7dd8e9c94ead6ff33fe725
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-02-21 10:08:01 +0800

    Fix typo in code

--- ABP-Playback.ps1
+++ ABP-Playback.ps1
@@ -1,8 +1,8 @@
 # Handles everything related to playing the generated preview
 #—————————————————————————————————————————————————————————————————————————————+—————————————————————
 $wpf.Button_Play.Add_Click({
-    $wpf.Media_Preivew.LoadedBehavior = "Manual"
-    $wpf.Media_Preivew.UnloadedBehavior = "Manual"
+    $wpf.Media_Preview.LoadedBehavior = "Manual"
+    $wpf.Media_Preview.UnloadedBehavior = "Manual"
     $wpf.Media_Preview.Source = "$TempLocation\result.avi"
     $wpf.Media_Preview.Play()
 })
