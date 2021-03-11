53ec98e1dd3051cebb0ff95727fa17e5e5339e5d
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-03-11 16:25:15 +0800

    Implement ability to call by CLI

--- ABP.ps1
+++ ABP.ps1
@@ -1,7 +1,25 @@
-# VARIABLES
-$ffmpegLocation = 'E:\Path\ffmpeg-4.3.2-2021-02-20-essentials_build\bin\ffmpeg.exe'
-# $ffmpegLocation = 'INSERT-PATH-TO-FFMPEG\ffmpeg.exe'
-$tempLocation = "$PSScriptRoot\Temp\"
-
 #—————————————————————————————————————————————————————————————————————————————+—————————————————————# Import file
-& '.\ABP-UILoader.ps1'
+Param (
+    [Parameter()][String] $InPath,
+    [Parameter()][Int] $ScreenW,
+    [Parameter()][Int] $ScreenH,
+    [Parameter()][Int] $BootTime,
+    [Parameter()][Int] $Repeat
+)
+
+# Variables
+# $script:ffmpegLocation = 'E:\Path\ffmpeg-4.3.2-2021-02-20-essentials_build\bin\ffmpeg.exe'
+$script:ffmpegLocation = 'INSERT-PATH-TO-FFMPEG\ffmpeg.exe'
+$script:tempLocation = "$PSScriptRoot\Temp\"
+
+if ($null -eq $InPath) {
+    & '.\ABP-UILoader.ps1'
+
+} else {
+     & '.\ABP-CLI.ps1' `
+        -InPath   $InPath `
+        -ScreenW  $ScreenW `
+        -ScreenH  $ScreenH `
+        -BootTime $BootTime `
+        -Repeat   $Repeat
+}
