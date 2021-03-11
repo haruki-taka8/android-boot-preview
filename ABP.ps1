a4c42931f1c8eb61d70f1cf28a91be2f14e87442
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-03-11 16:02:20 +0800

    Ahoy! Initial commit!

---
+++ ABP.ps1
@@ -0,0 +1,7 @@
+# VARIABLES
+$ffmpegLocation = 'E:\Path\ffmpeg-4.3.2-2021-02-20-essentials_build\bin\ffmpeg.exe'
+# $ffmpegLocation = 'INSERT-PATH-TO-FFMPEG\ffmpeg.exe'
+$tempLocation = "$PSScriptRoot\Temp\"
+
+#—————————————————————————————————————————————————————————————————————————————+—————————————————————# Import file
+& '.\ABP-UILoader.ps1'
